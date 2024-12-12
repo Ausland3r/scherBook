058aece0990e1c70b639cc7fb34fbe2b557c8193
Ausland3r <Ausland3r@yandex.ru>
2024-12-12 21:16:44 +0300

    user change

--- src/models.js
+++ src/models.js
@@ -5,9 +5,7 @@ const userSchema = new mongoose.Schema({
   email: { type: String, required: true },
   username: { type: String, required: true },
   password: { type: String, required: true },
-  role: { type: String, enum: ["user", "admin"], required: true },
-  available_books: { type: [String], default: [] },
-  exchange_history: { type: [String], default: [] },
+  role: { type: String, enum: ["user", "admin"], required: true }
 });
 
 const User = mongoose.model("User", userSchema, "Users"); // Явно указываем имя коллекции
