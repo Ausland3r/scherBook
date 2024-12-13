9e5b1fa44519194958881dca0cff888b36341a8e
Ausland3r <Ausland3r@yandex.ru>
2024-12-13 17:28:50 +0300

    contactData

--- src/models.js
+++ src/models.js
@@ -5,7 +5,8 @@ const userSchema = new mongoose.Schema({
   username: { type: String, required: true },
   password: { type: String, required: true },
   role: { type: String, enum: ["user", "admin"], required: true },
-  profileImage: { type: String, default: "" }
+  profileImage: { type: String, default: "" },
+  contactData: {type: String, default: ""}
 });
 
 const User = mongoose.model("User", userSchema, "Users");
