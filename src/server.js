2de5322784c9a83d3c02d8a6c2cd55e57573b068
Ausland3r <Ausland3r@yandex.ru>
2024-12-15 16:39:56 +0300

    cors added

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ require("dotenv").config({ path: "/etc/secrets/.env" });
 
 const mongoose = require("mongoose");
 const express = require("express");
+const cors = require("cors");
 const routes = require("./routes"); // Импорт маршрутов
 
 const app = express();
@@ -17,9 +18,18 @@ mongoose
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("Failed to connect to MongoDB", err));
 
+  const corsOptions = {
+    origin: "*", // Разрешить запросы с любого источника
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Разрешённые методы
+    allowedHeaders: ["Content-Type", "Authorization"], // Разрешённые заголовки
+    credentials: false // Если не нужно передавать токены/куки
+  };
+
 // Middleware
 app.use(express.json()); // Обработка JSON-запросов
 app.use("/api", routes); // Подключение маршрутов по префиксу "/api"
+app.use(cors(corsOptions));
+
 
 // Запуск сервера
 app.listen(PORT, () => {
